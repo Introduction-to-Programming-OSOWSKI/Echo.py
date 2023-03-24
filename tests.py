b50e5a4e5627d4a6d3c874968de9eba04c2f91c4
Zack Osowski <62389933+zackosowski@users.noreply.github.com>
2023-03-24 13:37:17 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@
 import main;
 import datetime;
 
-year = 2022
-month = 9
-day = 17
+year = 2023
+month = 4
+day = 1
 
 def test_code():
     assert main.echo("yo",2) == "yoyo", 'echo("yo", 2) failed'
